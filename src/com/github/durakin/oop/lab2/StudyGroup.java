13d31b6ce61d885b8cbf00a0a0aea2ffc26e7f19
durakin <albertnepomnyashiy@gmail.com>
2020-10-08 03:11:15 +0700

    Lab 2 seems to be ready

---
+++ src/com/github/durakin/oop/lab2/StudyGroup.java
@@ -0,0 +1,46 @@
+package com.github.durakin.oop.lab2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class StudyGroup {
+    private final String NAME;
+    private final List<Student> STUDENTS;
+
+    public StudyGroup(String name) {
+        NAME = name;
+        STUDENTS = new ArrayList<>();
+    }
+
+    public void addStudent(Student student) {
+        this.STUDENTS.add(student);
+    }
+
+    public double score(){
+        double result = 0;
+        for (Student i:
+             this.STUDENTS) {
+            result += i.score();
+        }
+        return result/this.STUDENTS.size();
+    }
+
+    public String getNAME() {
+        return NAME;
+    }
+
+    public List<Student> getSTUDENTS() {
+        return STUDENTS;
+    }
+
+    @Override
+    public String toString() {
+        var students = "";
+        for (Student i : STUDENTS) {
+            students += i + "\n";
+        }
+        return "StudyGroup " + NAME + "\nStudents:\n" + students;
+    }
+
+}
